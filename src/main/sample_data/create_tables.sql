ce789eeb9db89f443745d48824be795d57e9baaf
Kattkovacs <kattkovacs@gmail.com>
2020-08-25 16:34:21 +0200

    create order, order_details table

--- src/main/sample_data/create_tables.sql
+++ src/main/sample_data/create_tables.sql
@@ -5,6 +5,7 @@ ALTER TABLE IF EXISTS ONLY public.product DROP CONSTRAINT IF EXISTS product_cate
 ALTER TABLE IF EXISTS ONLY public.product DROP CONSTRAINT IF EXISTS product_pk CASCADE;
 ALTER TABLE IF EXISTS ONLY public.user DROP CONSTRAINT IF EXISTS user_pk CASCADE;
 ALTER TABLE IF EXISTS ONLY public.order DROP CONSTRAINT IF EXISTS order_pk CASCADE;
+ALTER TABLE IF EXISTS ONLY public.order_details DROP CONSTRAINT IF EXISTS order_details_pk CASCADE;
 
 
 DROP TABLE IF EXISTS public.supplier;
@@ -57,21 +58,30 @@ CREATE TABLE public.user (
                                 password text NOT NULL
 );
 
+
 DROP TABLE IF EXISTS public.order;
 CREATE TABLE public.order (
-                                id serial NOT NULL
-                                   constraint order_pk
-                                       primary key,
-                                phone text NOT NULL,
-                                address text NOT NULL,
-                                city text NOT NULL,
-                                state text NOT NULL,
-                                zip int NOT NULL,
-                                address2 text NOT NULL,
-                                city2 text NOT NULL,
-                                state2 text NOT NULL,
-                                zip2 int NOT NULL
+                              id serial NOT NULL
+                                  constraint order_pk
+                                      primary key,
+                              total_order_price float8 NOT NULL
+);
 
+DROP TABLE IF EXISTS public.order_details;
+CREATE TABLE public.order_details (
+                                id serial NOT NULL
+                                   constraint order_details_pk
+                                       primary key,
+                                order_id integer NOT NULL
+                                    constraint order_id_fk
+                                        references order
+                                            on delete cascade,
+                                product_id integer NOT NULL
+                                    constraint product_id_fk
+                                        references product
+                                            on delete cascade,
+                                quantity integer NOT NULL,
+                                total_product_price float8 NOT NULL
 );
 
 
