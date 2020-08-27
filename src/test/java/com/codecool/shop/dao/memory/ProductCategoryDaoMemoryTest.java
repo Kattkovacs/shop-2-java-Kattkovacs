f2ad002658a5545d565f391d6830582165053693
Kattkovacs <kattkovacs@gmail.com>
2020-08-27 12:05:16 +0200

    add supplier tests

--- src/test/java/com/codecool/shop/dao/memory/ProductCategoryDaoMemoryTest.java
+++ src/test/java/com/codecool/shop/dao/memory/ProductCategoryDaoMemoryTest.java
@@ -2,9 +2,14 @@ package com.codecool.shop.dao.memory;
 import com.codecool.shop.dao.implementation_memory.ProductCategoryDaoMem;
 import com.codecool.shop.dao.interfaces.ProductCategoryDao;
 import com.codecool.shop.model.ProductCategory;
-import org.junit.Test;
+import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 public class ProductCategoryDaoMemoryTest {
 
@@ -25,4 +30,40 @@ public class ProductCategoryDaoMemoryTest {
     public void add_addNewProductCategory_doesNotThrowAnException() {
         assertDoesNotThrow(() -> productCategoryDao.add(productCategoryOne));
     }
+
+    @Test
+    public void add_addSupplierWithNullArgument_throwIllegalArgumentException() {
+        ProductCategory productCategoryThree = new ProductCategory(null, "Department", "Description");
+        ProductCategory productCategoryFour = new ProductCategory("Name", null, "Description");
+        ProductCategory productCategoryFive = new ProductCategory("Name", "Department", null);
+        assertThrows(IllegalArgumentException.class, () -> productCategoryDao.add(productCategoryThree));
+        assertThrows(IllegalArgumentException.class, () -> productCategoryDao.add(productCategoryFour));
+        assertThrows(IllegalArgumentException.class, () -> productCategoryDao.add(productCategoryFive));
+    }
+
+//    @Test
+//    public void find_findFirstId_returnTestSupplierOne() {
+//        Supplier supplier = supplierDao.find(1);
+//        assertEquals(supplier, supplierOne);
+//    }
+//
+//    @Test
+//    public void find_findSecondId_returnTestSupplierTwo() {
+//        Supplier supplier = supplierDao.find(2);
+//        assertEquals(supplier, supplierTwo);
+//    }
+//
+//    @Test
+//    public void remove_removeFirstSupplier_getNull(){
+//        supplierDao.remove(1);
+//        Supplier supplier = supplierDao.find(1);
+//        assertNull(supplier);
+//    }
+//
+//    @Test
+//    public void getAll_getAllSuppliers_hasLengthTwo() {
+//        List<Supplier> suppliers = supplierDao.getAll();
+//        assertEquals(suppliers.size(), 2);
+//    }
+
 }
--- src/test/java/com/codecool/shop/dao/memory/SupplierDaoMemoryTest.java
+++ src/test/java/com/codecool/shop/dao/memory/SupplierDaoMemoryTest.java
@@ -5,6 +5,9 @@ import com.codecool.shop.dao.interfaces.SupplierDao;
 import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SupplierDaoMemoryTest {
@@ -17,12 +20,40 @@ public class SupplierDaoMemoryTest {
          supplierDao = SupplierDaoMem.getInstance();
          supplierOne = new Supplier("Test Supplier One", "Test Supplier One Description");
          supplierTwo = new Supplier("Test Supplier Two", "Test Supplier Two Description");
-         supplierDao.add(supplierOne);
-         supplierDao.add(supplierTwo);
+         supplierOne.setId(1);
+         supplierTwo.setId(2);
     }
 
     @Test
     public void add_addNewSupplier_doesNotThrowAnException() {
         assertDoesNotThrow(() -> supplierDao.add(supplierOne));
     }
+
+    @Test
+    public void add_addSupplierWithNullArgument_throwIllegalArgumentException() {
+        Supplier supplierThree = new Supplier(null, "Description");
+        Supplier supplierFour = new Supplier("Name", null);
+        assertThrows(IllegalArgumentException.class, () -> supplierDao.add(supplierThree));
+        assertThrows(IllegalArgumentException.class, () -> supplierDao.add(supplierFour));
+    }
+
+    @Test
+    public void find_findFirstId_returnTestSupplierOne() {
+        Supplier supplier = supplierDao.find(1);
+        assertEquals(supplierOne.getId(), supplier.getId());
+    }
+
+    @Test
+    public void remove_removeFirstSupplier_getNull(){
+        supplierDao.remove(1);
+        Supplier supplier = supplierDao.find(1);
+        assertNull(supplier);
+    }
+
+    @Test
+    public void getAll_getAllSuppliers_hasLengthTwo() {
+        List<Supplier> suppliers = supplierDao.getAll();
+        assertEquals( 1, suppliers.size());
+    }
+
 }
